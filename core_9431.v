2bf0ff798d4b923713f3f0bdc7bf3e084de20143
zaileshar <zaileshar@gmail.com>
2025-12-03 18:22:01 +0530

    add unit tests

---
+++ core_9431.v
@@ -0,0 +1,13 @@
+module core_fifo #(parameter DEPTH=16) (
+    input clk, rst, wr_en, rd_en,
+    input [7:0] din,
+    output [7:0] dout,
+    output full, empty
+);
+    reg [7:0] mem [0:DEPTH-1];
+    reg [3:0] wptr, rptr;
+    always @(posedge clk) if (wr_en && !full) mem[wptr] <= din;
+    assign dout = mem[rptr];
+    assign full = (wptr + 1'b1 == rptr);
+    assign empty = (wptr == rptr);
+endmodule
