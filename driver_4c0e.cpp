fb73456d38c30ee5b54f4a6f1004d1fcc238cc0f
zaileshar <zaileshar@gmail.com>
2025-12-04 14:23:40 +0530

    fix bug in auth

---
+++ driver_4c0e.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+
+class DriverDriver {
+public:
+    DriverDriver(uint32_t base_addr) : base_addr(base_addr) {}
+    void write_reg(uint32_t offset, uint32_t val) {
+        std::cout << "Writing " << val << " to 0x" << std::hex << base_addr + offset << std::endl;
+    }
+private:
+    uint32_t base_addr;
+};
