93d3240394bb0d8913fa86a8d53cbe1b1468347b
zaileshar <zaileshar@gmail.com>
2025-12-08 16:02:57 +0530

    initial commit

---
+++ fifo_7df5.v
@@ -0,0 +1,13 @@
+module fifo (
+    input clk,
+    input rst_n,
+    input [7:0] data_in,
+    output reg [7:0] data_out
+);
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            data_out <= 8'h0;
+        else
+            data_out <= data_in + 8'h1;
+    end
+endmodule
